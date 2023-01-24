4b0dc3f10ca8b5688f47dcf7ce160dddedc681f3
Christian Cecilia <christian.cecilia1@gmail.com>
2023-01-23 21:44:33 -0500

    project skeleton

--- hls-stream-reader/src/lib.rs
+++ hls-stream-reader/src/lib.rs
@@ -1,14 +1,103 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::{env::temp_dir, error::Error, fmt, fs, io, path::PathBuf};
+
+#[derive(Debug)]
+pub enum StreamUrlError {
+    InvalidProtocol,
+    InvalidEndingExtension,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Error for StreamUrlError {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl fmt::Display for StreamUrlError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            StreamUrlError::InvalidProtocol => {
+                write!(f, "Invalid protocol use in uri MUST be http or https")
+            }
+            StreamUrlError::InvalidEndingExtension => {
+                write!(f, "Invalid ending extension HLS stream MUSt end in .m3u8")
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum PlaylistDownloadError {
+    RequestFailed,
+    FailedToReadResponse,
+    FailedToCreateTempFile,
+    FailedToCopyData,
+}
+
+impl Error for PlaylistDownloadError {}
+
+impl fmt::Display for PlaylistDownloadError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            PlaylistDownloadError::RequestFailed => write!(f, "request for playlist failed"),
+            PlaylistDownloadError::FailedToReadResponse => write!(f, "Failed to read request data"),
+            PlaylistDownloadError::FailedToCreateTempFile => {
+                write!(f, "Failed to create temp file")
+            }
+            PlaylistDownloadError::FailedToCopyData => {
+                write!(f, "Failed to copy playlist to temp file")
+            }
+        }
+    }
+}
+
+pub struct StreamUrl {
+    pub uri: String,
+}
+
+impl StreamUrl {
+    pub fn new(uri: String) -> Self {
+        StreamUrl { uri }
+    }
+
+    pub fn is_valid(&self) -> Result<(), StreamUrlError> {
+        if !self.uri.starts_with("http") && !self.uri.starts_with("https") {
+            return Err(StreamUrlError::InvalidProtocol);
+        }
+
+        if !self.uri.ends_with(".m3u8") {
+            return Err(StreamUrlError::InvalidEndingExtension);
+        }
+
+        Ok(())
+    }
+
+    pub async fn download_to_file(
+        &self,
+        specific_file_path: Option<PathBuf>,
+    ) -> Result<PathBuf, PlaylistDownloadError> {
+        let file_path;
+
+        if let Some(specific_file_path) = specific_file_path {
+            file_path = specific_file_path;
+        } else {
+            let temp_dir = temp_dir();
+            file_path = temp_dir.join("downloaded_playlist.m3u8");
+        }
+
+        let temp_file_result = fs::File::create(&file_path);
+
+        let Ok(response) = reqwest::get(&self.uri).await else {
+            return Err(PlaylistDownloadError::RequestFailed);
+        };
+
+        let Ok(response_data) = response.text().await else {
+            return Err(PlaylistDownloadError::FailedToReadResponse);
+        };
+
+        let Ok(mut temp_file) = temp_file_result else {
+            return Err(PlaylistDownloadError::FailedToCreateTempFile);
+        };
+
+        if io::copy(&mut response_data.as_bytes(), &mut temp_file).is_err() {
+            return Err(PlaylistDownloadError::FailedToCopyData);
+        };
+
+        Ok(file_path)
     }
 }
--- rusty-hls/src/main.rs
+++ rusty-hls/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use hls_stream_reader::StreamUrl;
+use std::{path::PathBuf, process::exit};
+
+#[derive(Parser)]
+#[command(name = "HLS Parser")]
+#[command(author = "Christian Cecilia <christian.cecilia1@gmail.com@gmail.com>")]
+#[command(version = "0.0.1")]
+#[command(about = "Parses an HLS stream for all relevant data and serializes tags", long_about = None)]
+struct CLI {
+    #[arg(long, help = "Stream url to fetch manifest from.")]
+    stream_url: Option<String>,
+    #[arg(long, help = "Stream url to fetch manifest from.")]
+    master_file: Option<String>,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli_args = CLI::parse();
+    let hls_file_path: PathBuf;
+
+    if let Some(url) = cli_args.stream_url {
+        let stream_url = StreamUrl::new(url);
+        if let Err(e) = stream_url.is_valid() {
+            eprint!("{}", e);
+            exit(1);
+        }
+
+        let file_download = stream_url.download_to_file(None).await;
+
+        if let Ok(file_download) = file_download {
+            hls_file_path = file_download;
+        }
+    } else if let Some(file_path) = cli_args.master_file {
+        hls_file_path = PathBuf::from(file_path);
+    }
 }
